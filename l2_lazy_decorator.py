2fee834ac5a83047a5af55bf5d28d5fe98384c69
ovgolovin <ovgolovin@gmail.com>
2013-01-27 18:36:11 +0400

    Task 2. Lazy decoration - Added another usage

--- l2_lazy_decorator.py
+++ l2_lazy_decorator.py
@@ -9,6 +9,7 @@ def make_decorator_lazy(decorator):
     return apply_decorator
 
 def tests():
+    # Withouth decorator
     def special_heavy_decorator_for_print_function(f):
         print("Hi! I'm a brand-new print function decorator. I output the huge next message while decorating!")
         print("HUGE MESSAGE")
@@ -30,8 +31,32 @@ def tests():
 
     print('\n')
 
+    # With decorator (first variant of application: applying to decorator on its application)
     print('Using lazy decorator\n----')
     @make_decorator_lazy(special_heavy_decorator_for_print_function)
+    def print_function():
+        print("I (namely 'print_function', no-no, again 'PRINT_FUNCTION') have just printed this kewl piece of text.")
+    print('*Now the decorated function is about to be called.')
+    print_function()
+
+
+    # With decorator (second variant of application: decorated decorator; good if you want the decorator to become
+    # lazy forever)
+    @make_decorator_lazy
+    def special_heavy_decorator_for_print_function(f):
+        print("Hi! I'm a brand-new print function decorator. I output the huge next message while decorating!")
+        print("HUGE MESSAGE")
+
+        @wraps(f)
+        def new_decorated_f(*args, **kwargs):
+            print('This is output by decorated function before the real function is called.')
+            return f(*args, **kwargs)
+        return new_decorated_f
+
+    print('\n')
+
+    print('Using lazy decorator\n----')
+    @special_heavy_decorator_for_print_function
     def print_function():
         print("I (namely 'print_function', no-no, again 'PRINT_FUNCTION') have just printed this kewl piece of text.")
     print('*Now the decorated function is about to be called.')
