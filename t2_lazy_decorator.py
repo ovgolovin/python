26934330dbf382cf76a6613f2bce74292286ed3b
ovgolovin <ovgolovin@gmail.com>
2013-01-27 19:15:42 +0400

    Task 2. Lazy decoration - Corrected mistakes. Added corresponding test cases.

--- t2_lazy_decorator.py
+++ t2_lazy_decorator.py
@@ -4,12 +4,18 @@ from __future__ import division
 from functools import wraps
 
 def make_decorator_lazy(decorator):
-    def apply_decorator(f):
-        return lambda *args, **kwargs: decorator(f)(*args, **kwargs)
-    return apply_decorator
+    lazy_decorated = []
+    def new_lazy_decorator(f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            if not lazy_decorated:
+                lazy_decorated.append(decorator(f))
+            return lazy_decorated[0](*args, **kwargs)
+        return decorated
+    return new_lazy_decorator
 
 def tests():
-    # Withouth decorator
+    # Without decorator
     def special_heavy_decorator_for_print_function(f):
         print("Hi! I'm a brand-new print function decorator. I output the huge next message while decorating!")
         print("HUGE MESSAGE")
@@ -31,12 +37,25 @@ def tests():
 
     print('\n')
 
-    # With decorator (first variant of application: applying to decorator on its application)
+    # With decorator
+    # First variant of application: lazy decorator is applied to decorator every time decorator is applied.
+    # Not very good way of using it as decorator is applied every time the function is called
     print('Using lazy decorator\n----')
     @make_decorator_lazy(special_heavy_decorator_for_print_function)
     def print_function():
         print("I (namely 'print_function', no-no, again 'PRINT_FUNCTION') have just printed this kewl piece of text.")
     print('*Now the decorated function is about to be called.')
+    print_function()
+
+
+    print('\n\n')
+    print('Decorator here should be applied again')
+
+    @make_decorator_lazy(special_heavy_decorator_for_print_function)
+    def print_function():
+        print("I (namely 'print_function', no-no, again 'PRINT_FUNCTION') have just printed this kewl piece of text.")
+    print('*Now the decorated function is about to be called.')
+
     print_function()
 
 
@@ -56,6 +75,14 @@ def tests():
     print('\n')
 
     print('Using lazy decorator\n----')
+    @special_heavy_decorator_for_print_function
+    def print_function():
+        print("I (namely 'print_function', no-no, again 'PRINT_FUNCTION') have just printed this kewl piece of text.")
+    print('*Now the decorated function is about to be called.')
+    print_function()
+
+    print('\n\n')
+    print('Decorator should NOT be applied again')
     @special_heavy_decorator_for_print_function
     def print_function():
         print("I (namely 'print_function', no-no, again 'PRINT_FUNCTION') have just printed this kewl piece of text.")
