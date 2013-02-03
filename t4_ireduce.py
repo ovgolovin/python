332b8baab7e08bb06e1fcacf58a5f218e924a38a
ovgolovin <ovgolovin@gmail.com>
2013-02-03 04:55:56 +0400

    initial value

--- t4_ireduce.py
+++ t4_ireduce.py
@@ -3,11 +3,19 @@ from __future__ import division
 from operator import mul
 import unittest
 
-def ireduce(function, iterable, initializer=None):
+def ireduce(function, iterable, *initializer):
     """
     Iterator yielding all values (including intermediate),
     with the last value equal to the output of traditional reduce.
     """
+    if len(initializer) > 1:
+        raise TypeError("You have provided more than 3 arguments.")
+
+    if initializer:
+        initializer = initializer[0]
+    else:
+        initializer = None
+
     it = iter(iterable)
     if initializer is None:
         try:
@@ -22,3 +30,9 @@ def ireduce(function, iterable, initializer=None):
 class Tests(unittest.TestCase):
     def test_trivial(self):
         self.assertEqual(list(ireduce(mul, [1,2,3,4,5], 1)), [1, 2, 6, 24, 120])
+
+    def test_should_raise_if_more_than_four_parameters(self):
+        self.assertRaises(TypeError, ireduce(mul, [1,2,3,4,5], 1, 1))
+
+    def test_should_raise_if_less_then_two_parameters(self):
+        self.assertRaises(TypeError, ireduce, mul)
